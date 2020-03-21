49946d6ab16219131ca5a14310daef28f952c9af
Rémi Tiab <remi.tiab@lasuiteandco.com>
2020-03-21 23:53:54 +0100

    STEP 10

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.get('/', function(req, res) {
   res.render('index', { title: 'My events' });
 });
 
+app.post('/party', function(req, res) {
+  res.send('Post ok !');
+});
+
 app.listen(process.env.PORT, () =>
   console.log(`PWA <My Events> listening on port ${process.env.PORT}!`)
 );
