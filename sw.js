faf039fcc668161bdbac6a27847f578960e0d3d5
Rémi Tiab <remi.tiab@lasuiteandco.com>
2020-03-22 13:51:26 +0100

    STEP 33

--- sw.js
+++ sw.js
@@ -1,3 +1,6 @@
 addEventListener('install', event => {
   console.log('Hello from the service worker');
+  if (event.request.headers.get('Accept').includes('text/html')) {
+    event.respondWith(fetch(event.request));
+  }
 });
